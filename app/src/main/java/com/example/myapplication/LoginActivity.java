0388d5a846f0a8a4f4d9dbfab7a3cc53d03b4000
pjrodz4 <pjrodz4@gmail.com>
2019-07-17 10:44:19 -0700

    FB login button working

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -23,16 +23,34 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+<<<<<<< Updated upstream
+=======
+
+        // initialize Firebase authorization and Callback manager
+        mAuth = FirebaseAuth.getInstance();
+>>>>>>> Stashed changes
         callbackManager = CallbackManager.Factory.create();
 
+        // sync login button
         loginButton = (LoginButton) findViewById(R.id.login_button);
+<<<<<<< Updated upstream
         loginButton.setReadPermissions(Arrays.asList(EMAIL));
+=======
+        // request permission to access email and public profile
+        loginButton.setReadPermissions("email", "public_profile");
+>>>>>>> Stashed changes
 
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+<<<<<<< Updated upstream
                 // App code
+=======
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                // check login status
+                handleFacebookAccessToken(loginResult.getAccessToken());
+>>>>>>> Stashed changes
             }
 
             @Override
@@ -45,11 +63,16 @@ public class LoginActivity extends AppCompatActivity {
                 // App code
             }
         });
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // pass the login results via callbackManager
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
     }
