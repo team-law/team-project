41c4902975ea71df143b5f17500036701087ef30
Andreea Ifrim <andreeaifrim@fb.com>
2019-07-29 12:23:25 -0700

    home view events ordered by time

--- app/src/main/java/com/example/myapplication/Fragments/AlbumFragment.java
+++ app/src/main/java/com/example/myapplication/Fragments/AlbumFragment.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.Fragments;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -19,6 +20,7 @@ import com.example.myapplication.Models.UserNode;
 import com.example.myapplication.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -36,6 +38,7 @@ public class AlbumFragment extends Fragment {
     public static final String TAG = "AlbumFragment";
     protected AlbumAdapter adapter;
     protected List<Event> mEvents;
+    Map<String, Boolean> userEvents = new HashMap<>(1);
     private SwipeRefreshLayout swipeContainer;
     private EndlessRecyclerViewScrollListener scrollListener;
 
@@ -127,30 +130,58 @@ public class AlbumFragment extends Fragment {
     protected void queryPosts() {
 
         DatabaseReference ref = database.getReference();
+        DatabaseReference myEventRef = ref.child("Events");
 
         // Generate a reference to a new location and add some data using push()
         // DatabaseReference pushedPostRef = postsRef.push();
 
-        //get the list of events, go through the events and only publish the ones from the list
         ref.addValueEventListener(new ValueEventListener() {
             FirebaseAuth mAuth = FirebaseAuth.getInstance();
             FirebaseUser user = mAuth.getCurrentUser();
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 UserNode userInfo = dataSnapshot.child("UserNodes").child(user.getUid()).getValue(UserNode.class);
-                Map<String, Boolean> userEvents = new HashMap<>(1);
                 userEvents = userInfo.eventsAttending; //gets map of events the user is attending
+            }
 
-                for(DataSnapshot eventSnapshot: dataSnapshot.child("Events").getChildren()){
-                    Event event = eventSnapshot.getValue(Event.class);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        //ordering by "smallest" first RN
+        //want it stored in the order: year, month, date //ADD ZEROS TO IT
+        //get the list of events, go through the events and only publish the ones from the list
+        myEventRef.orderByChild("date").addChildEventListener(new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
+
+                //for(DataSnapshot eventSnapshot: dataSnapshot.getChildren()){
+                    Event event = dataSnapshot.getValue(Event.class);
 
                     if (userEvents.containsKey(event.accessCode)) { //code from user list of events//
                         mEvents.add(event);
                         adapter.notifyDataSetChanged();
                     }
                     swipeContainer.setRefreshing(false);
-                }
+                //}
                 Log.d(TAG, "loaded events correctly");
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
             }
 
             @Override
